8c2a882bbff95c5e86a4bfc9376c998f5a07c5e1
Hunaid Memon <hunaid.hanif@gmail.com>
2022-06-21 17:30:30 +0500

    create user module & init middleware

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect to MongoDB
 connectDB()
 
+// Init Middleware
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the Library Management App' }));
 
 // Define routes here
