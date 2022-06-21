0adace2759b1d367dfee1f8f27c6e62b5254c469
Hunaid Memon <hunaid.hanif@gmail.com>
2022-06-21 14:06:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the Library Management App' }))
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server start on Port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on Port ${PORT}`))
